960ebbb7a3456370b968a0259c8eec0943263c46
fastik1 <davidbistrov@yandex.ru>
2023-06-17 23:12:01 +0300

    Add and change tests
    - Добавлены и исправлены тесты для методов: on() message() privateMessage() chatMessage()

--- tests/VkBotClassTests/VkBotTest.php
+++ tests/VkBotClassTests/VkBotTest.php
@@ -5,6 +5,8 @@ namespace Fastik1\Vkfast\Tests\VkBotClassTests;
 use Fastik1\Vkfast\Api\VkApi;
 use Fastik1\Vkfast\Bot\Events\Confirmation;
 use Fastik1\Vkfast\Bot\Events\MessageNew;
+use Fastik1\Vkfast\Bot\Rules\IsChatMessageRule;
+use Fastik1\Vkfast\Bot\Rules\IsPrivateMessageRule;
 use Fastik1\Vkfast\Bot\VkBot;
 use PHPUnit\Framework\TestCase;
 
@@ -66,25 +68,94 @@ class VkBotTest extends TestCase
         $this->bot = new VkBot($this->api);
     }
 
+    protected function tearDown(): void
+    {
+        unset($this->api, $this->bot);
+    }
+
+    public function test_on_method()
+    {
+        $this->assertCount(0, $this->bot->getHandlers());
+
+        $this->bot->on(MessageNew::class, function (MessageNew $event) {});
+
+        $this->assertCount(1, $this->bot->getHandlers());
+    }
+
+    public function test_message_method()
+    {
+        $this->assertCount(0, $this->bot->getHandlers());
+
+        $this->bot->message(function (MessageNew $event) {});
+
+        $this->assertCount(1, $this->bot->getHandlers());
+        $this->assertArrayNotHasKey('rules', $this->bot->getHandlers()[0]);
+    }
+
+    public function test_privateMessage_method()
+    {
+        $this->assertCount(0, $this->bot->getHandlers());
+
+        $this->bot->privateMessage(function (MessageNew $event) {});
+
+        $this->assertCount(1, $this->bot->getHandlers());
+        $this->assertArrayHasKey('rules', $this->bot->getHandlers()[0]);
+
+        $is_finded_rule = false;
+        foreach ($this->bot->getHandlers()[0]['rules'] as $rule) {
+            if (IsPrivateMessageRule::class == $rule::class) {
+                $is_finded_rule = true;
+            }
+        }
+        $this->assertTrue($is_finded_rule);
+    }
+
+    public function test_chatMessage_method()
+    {
+        $this->assertCount(0, $this->bot->getHandlers());
+
+        $this->bot->chatMessage(function (MessageNew $event) {});
+
+        $this->assertCount(1, $this->bot->getHandlers());
+        $this->assertArrayHasKey('rules', $this->bot->getHandlers()[0]);
+
+        $is_finded_rule = false;
+        foreach ($this->bot->getHandlers()[0]['rules'] as $rule) {
+            if (IsChatMessageRule::class == $rule::class) {
+                $is_finded_rule = true;
+            }
+        }
+        $this->assertTrue($is_finded_rule);
+    }
+
     public function test_event_message_new_handler()
     {
-        $this->expectOutputString('message_new' . self::OK);
+        $this->expectOutputString(self::OK);
 
-        $this->bot->on(MessageNew::class, function (MessageNew $event) {
-            echo $event->type;
+        $is_call = false;
+
+        $this->bot->on(MessageNew::class, function (MessageNew $event) use (&$is_call) {
+            $is_call = true;
         });
 
         $this->bot->run($this->event_message_new);
+
+        $this->assertTrue($is_call);
     }
 
     public function test_event_confirmation_handler()
     {
         $this->expectOutputString('confirmation');
 
-        $this->bot->on(Confirmation::class, function (Confirmation $event) {
+        $is_call = false;
+
+        $this->bot->on(Confirmation::class, function (Confirmation $event) use (&$is_call) {
+            $is_call = true;
             return 'confirmation';
         });
 
         $this->bot->run($this->event_confirmation);
+
+        $this->assertTrue($is_call);
     }
 }
