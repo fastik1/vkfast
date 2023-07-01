4ff23ae9d4369f5e2e8563737192a02a8e7a4824
fastik1 <davidbistrov@yandex.ru>
2023-07-01 17:13:50 +0300

    feat(message-new): добавить метод answer() в событие message_new
    Добавить новый метод answer() в событие message_new для возможности быстрого ответа в поступивший чат. Под капотом использовать $api->sendMessage()

--- src/Bot/Events/MessageNew.php
+++ src/Bot/Events/MessageNew.php
@@ -4,11 +4,16 @@
 namespace Fastik1\Vkfast\Bot\Events;
 
 
+use Fastik1\Vkfast\Api\Keyboard\Keyboard;
+
 /**
  * @property \Fastik1\Vkfast\Bot\Events\Objects\Message $message
  * @property \Fastik1\Vkfast\Bot\Events\Objects\ClientInfo $client_info
  */
 class MessageNew extends Event
 {
-
+    public function answer(int|string $message, ?Keyboard $keyboard = null, ?string $attachment = null, ...$arguments): mixed
+    {
+        return $this->api->sendMessage($this->message->peer_id, $message, $keyboard, $attachment, $arguments);
+    }
 }
